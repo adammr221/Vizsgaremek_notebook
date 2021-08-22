ec7a200040f886b9608475460ed0e8f3de1fb2d1
Ádám Mészáros <adammr221gmail.com>
2021-08-22 15:25:41 +0200

    headless mode on

--- src/test/java/testbase/baseTest.java
+++ src/test/java/testbase/baseTest.java
@@ -20,7 +20,7 @@ public class baseTest {
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("start-maximized");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
